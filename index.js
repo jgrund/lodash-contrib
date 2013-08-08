6be83921472f2e588e8c6432beb8820f9872ab93
Joe Grund <joe.grund@intel.com>
2013-08-08 16:30:56 -0400

    require(lodash) -> require('lodash').

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var underscorePath = require.resolve('underscore-contrib/node_modules/underscore');
 var lodashPath = require.resolve('lodash/dist/lodash');
 var cache = require.cache;
-var lodash = require(lodash);
+var lodash = require('lodash');
 
 cache[underscorePath] = cache[lodashPath];
 
